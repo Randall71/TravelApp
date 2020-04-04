3d3de9cc55ba57f5bf4f4495ea28045fd5e87a46
Randall71 <yannbilos@gmail.com>
2020-04-04 15:48:02 +0000

    Ajout du fichier style pour la gestion des différentes couleurs

---
+++ styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet , } from 'react-native';
+
+
+export const colors = {
+    darkBg: "#222",
+    lighBg: "#333",
+    darkHl: "#666",
+    lightHL: "#888",
+    pink: "#ea3372",
+    text: "#fff",
+    textSec: "#aaa"
+}
