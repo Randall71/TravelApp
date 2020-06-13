158429b575d62f7ba8c55b8808a95b2f4c02b6c5
Randall71 <yannbilos@gmail.com>
2020-06-13 22:16:51 +0000

    Finalisation du composant Photos

--- Screens/ProfileView/Components/Photos.js
+++ Screens/ProfileView/Components/Photos.js
@@ -1,31 +1,53 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 import {gs, colors} from '../../../styles'
 
 
-// const photos = [
-//     require("../../../assets/travel1.jpg"),
-//     require("../../../assets/travel2.jpg"),
-//     require("../../../assets/travel3.jpg"),
-//     require("../../../assets/travel4.jpg"),
-//     require("../../../assets/travel5.jpg"),
-//     require("../../../assets/travel6.jpg"),
-//     require("../../../assets/travel7.jpg"),
-//     require("../../../assets/travel8.jpg"),
-// ]
+const photos = [
+    require("../../../assets/travel1.jpg"),
+    require("../../../assets/travel2.jpg"),
+    require("../../../assets/travel3.jpg"),
+    require("../../../assets/travel4.jpg"),
+    require("../../../assets/travel5.jpg"),
+    require("../../../assets/travel6.png"),
+    require("../../../assets/travel7.jpg"),
+    require("../../../assets/travel8.jpeg"),
+]
 
 
 export default function Photos(){
+
+  
     return(
-        <View style={gs.sectionContainer, {marginTop: 8}}>
-            <Text>Photos de Voyage</Text>
+        <View style={[gs.sectionContainer, {marginTop: 8 }]}>
+            <Text style={gs.sectionTitle}>Mes Photos</Text>
+
+            <View style={styles.photosContainer}>
+                {photos.map((photo, index)=>{
+                return <Image
+                    source={photo} 
+                    key={index} 
+                    style={[styles.photo,{
+                        marginRight: index + 1 % 3 === 0 ? 0 : 12
+                    }]} 
+                />
+            })}
+            </View>
+
         </View>
     )
 }
 
 
 const styles =  StyleSheet.create({
-    container:{
-
+    photosContainer:{
+        flexDirection: 'row',
+        flexWrap: "wrap",
+        marginTop: 16
+    },
+    photo:{
+        width: 85,
+        height: 85,
+        borderRadius: 8,
     }
 })
